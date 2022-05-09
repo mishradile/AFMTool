30a2cd43a08079daa7267e6eda9bf46ec11d5bdf
ChenJiakaiIME <chenjiakaiime@gmail.com>
2022-05-09 16:14:32 +0800

    Initial commit

---
+++ AFMTOOL/util/line_profile/line_profile.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
+
+def plot_line_profile(array, x1, y1, x2, y2):
+    """
+    Plot the height profile along a line across two neighbouring Cu contact points
+    """
+    
+    return 0
+
+def get_step_height(array, x1, y1, x2, y2):
+    
+    
+    return 0
